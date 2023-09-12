be94cb25ceff5afb1ce538febefc804fee02eda1
Savanna Piscitelli <savannapiscitelli@gmail.com>
2023-09-12 14:26:57 -0600

    Create StudentInfo and week2.js

---
+++ app/StudentInfo.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return (
+        <div>
+            <p>Name: Savanna Piscitelli</p>
+            <p>Course Section: CPRG 306 F</p> 
+            <Link href="https://github.com/savannapisc/cprg306-assignments">GitHub Repository: https://github.com/savannapisc/cprg306-assignments</Link>
+        </div>
+
+    );
+}
