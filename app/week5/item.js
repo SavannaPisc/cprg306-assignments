3d0872423b17c8194fcc5cf06da7b8d474058dab
Savanna Piscitelli <savannapiscitelli@gmail.com>
2023-10-03 12:53:01 -0600

    Create week 5 folder with page, item-list and item

---
+++ app/week5/item.js
@@ -0,0 +1,10 @@
+export default function Item({name, quantity, category}) {
+    return (
+        <main>
+            <ul className = "text-center p-2 m-4 bg-sky-700 rounded-md">
+                <li className = "text-xl font-bold">{name}</li>
+                <li>Buy {quantity} in {category}</li>
+            </ul>
+        </main>
+    );
+}
