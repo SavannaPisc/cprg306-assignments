0e04414ecff50363a5e79f972627f768c05d1306
Savanna Piscitelli <savannapiscitelli@gmail.com>
2023-10-05 10:08:24 -0600

    Fix name sorting in item-list.js

--- app/week5/item-list.js
+++ app/week5/item-list.js
@@ -15,22 +15,19 @@ export default function ItemList() {
     }));
  
     // Sort items by name or category depending on sortBy state variable
-    function  handleChange(sortBy) {
-        if (sortBy == "name") {
-            items = items.sort((a, b) => a.name.localeCompare(b.name));
-            alert("Sorted by name");
-        } else if (sortBy == "category") {
-            items = items.sort((a, b) => a.category.localeCompare(b.category));
-            alert("Sorted by category");
-        }
-    };
+    if (sortBy === "name") {
+        items = items.sort((a, b) => a.name.localeCompare(b.name));
+    } else if (sortBy === "category") {
+        items = items.sort((a, b) => a.category.localeCompare(b.category));
+        alert("Sorted by category");
+    }
 
     return ( 
         <>
             <h1 className = "text-center p-2 m-4 text-xl font-bold items-center">My Shopping List</h1>
             <div className = "flex flex-items-center justify-center w-full rounded-md">
-                <button id = "name" value = {sortBy} onClick = {(e) => handleChange(e.target.value)} class = "bg-sky-500 p-4 focus:bg-sky-300 m-8 w-40 rounded-sm">Sort by name</button>
-                <button id = "category" value = {sortBy} onClick = {(e) => handleChange(e.target.value)} class = "bg-sky-500 p-4 focus:bg-sky-300 m-8 w-40 rounded-sm">Sort by category</button>
+                <button id = "name" value = {sortBy} onClick = {(e) => setSortBy(e.target.value)} class = "bg-sky-500 p-4 focus:bg-sky-300 m-8 w-40 rounded-sm">Sort by name</button>
+                <button id = "category" value = {sortBy} onClick = {(e) => setSortBy(e.target.value)} class = "bg-sky-500 p-4 focus:bg-sky-300 m-8 w-40 rounded-sm">Sort by category</button>
 
             </div>
 
